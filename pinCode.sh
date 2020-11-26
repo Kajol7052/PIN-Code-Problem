b15b86fd46c605a25d8ad564f9c333cb2959be27
“Kajol7052” <kajol7052@gmail.com>
2020-11-26 12:20:25 +0530

    Added : PIN Code must contains 6 Digit

--- pinCode.sh
+++ pinCode.sh
@@ -1,3 +1,13 @@
 #!/bin/bash/
 
 echo "Welcome to PIN Code Problem"
+
+#UC-1 Validate 6 Digit PIN Code
+read -p "Enter word ending with things : " pin
+pat="#^[0-9]{6}$"
+if [[ $pin =~ $pat ]]
+then
+        echo "Valid PIN Code"
+else
+        echo "Invaild PIN Code"
+fi
